2d2159c1c392ef6553fd011aa429f9ffb8a792c0
Daniel <pepelacon@gmail.com>
2023-06-18 20:50:53 -0400

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,6 +39,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        
         {
           from_name: form.name,
           to_name: "Daniel",
